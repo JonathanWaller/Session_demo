9bf9233d3e7b32341e319231f14a9687b1973a55
JonathanWaller <waller.j27@gmail.com>
2018-09-26 10:07:34 -0500

    fixed typo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Login from "./compnents/Login";
+import Login from "./components/Login";
 
 class App extends Component {
   render() {
